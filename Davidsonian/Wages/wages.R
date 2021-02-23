d4cba6972ae199a4b899a306c9184f44f87bea7f
najordan1 <nathan.d.jordan@gmail.com>
2021-02-22 22:25:47 -0500

    Data update, sources added

--- Davidsonian/Wages/wages.R
+++ Davidsonian/Wages/wages.R
@@ -11,18 +11,20 @@ one <- ggplot(wages,aes(x=reorder(Institution,-Rate),y=Rate))+
            stat="identity",show.legend = F) + 
   scale_fill_manual(values=c("#377EB8","#E41A1C")) +
   labs(x="", y="Student Minimum Wage", 
-       title="Davidson's Student Minimum Wage Compared With Peers") +
-  geom_text(aes(label=Label), hjust=-0.125) +
+       title="Davidson's Student Minimum Wage Compared With Peers",
+       caption="Visualization and data collection by Nathan Jordan '22. Data are current as of February 22, 2021.") +
+  geom_text(aes(label=Label), hjust=1.1, color="white") +
   coord_flip() +
-  theme_minimal() #+
-  #theme(axis.text.x = element_text(angle=45, hjust=1, size=12, face="bold")) 
+  theme_minimal() +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
 
 two <- ggplot(wages,aes(x=reorder(Institution,-Endowment),y=Endowment))+
   geom_bar(aes(fill=ifelse(Institution=="Davidson", "Red", "Black")),
            stat="identity",show.legend = F) + 
   scale_fill_manual(values=c("#377EB8","#E41A1C")) +
   labs(x="", y="Endowment Value (2018)", 
-       title="Davidson's 2018 Endowment Value Compared With Peers") +
+       title="Davidson's 2018 Endowment Value Compared With Peers",
+       caption="Visualization by Nathan Jordan '22. Data from The Chronicle of Higher Education.") +
   geom_text(aes(label=Endowmentlabel), hjust=1.1, color="white") +
   coord_flip() +
   theme_minimal() +
