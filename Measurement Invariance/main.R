01710391208785afaef478bb00ee7b070cd5ae09
najordan1 <nathan.d.jordan@gmail.com>
2022-08-20 21:59:32 -0400

    generalizing multi cfa functions

--- Measurement Invariance/main.R
+++ Measurement Invariance/main.R
@@ -12,6 +12,9 @@ equality_constraints <- list(
   strict = c("loadings", "intercepts", "residuals")
 )
 
+# The fitness metrics we're interested in
+fit_indices <- c("chisq", "df", "pvalue", "cfi", "rmsea")
+
 #' Makes a likert graph displaying responses to question by a group,
 #' which should be a factor variable for a given question
 #' @author Nathan Jordan
@@ -25,11 +28,18 @@ equality_constraints <- list(
 #'
 #' @return a likert plot
 create_likert <- function(df, question, group, labels) {
-  # Determine correct (ish) labels and colors for responses
-  # Obviously, should be changed to correct labels in a given setting, could
-  # potentially refactor to be a parameter
+  # Determine the number of likert levels exist in this question
+  num_levels <- length(
+    df |>
+      select(question)|>
+      na.omit() |>
+      distinct() |>
+      pull()
+    )
+  
+  # Determine correct (ish) labels and colors for the likert scale items
+  # TODO: refactor to a parameter
   # Color palette from https://colorbrewer2.org/#type=diverging&scheme=RdBu&n=5
-  num_levels <- length(df |> select(question) |> na.omit() |> distinct() |> pull())
   if (num_levels == 5) {
     levels <- c("Strongly Disagree", "Disagree", "Neither Agree nor Disagree", "Agree", "Strongly Agree")
     colors <- c("#ca0020", "#f4a582", "#f7f7f7", "#92c5de", "#0571b0")
@@ -40,8 +50,10 @@ create_likert <- function(df, question, group, labels) {
   
   # Change responses to factor
   items <- df |>
-    mutate(q = levels[eval(sym(question))],
-           q = factor(q, levels = levels))
+    mutate(
+      q = levels[eval(sym(question))],
+      q = factor(q, levels = levels)
+    )
   
   grouping <- df$group
   items <- items |> select(q)
@@ -92,20 +104,33 @@ create_graphs_by_concept <- function(df, concept, questions, group, labels) {
 #' Create a multi-group confirmatory factor analysis model
 #' @author Nathan Jordan
 #'
+#' @param df - the dataframe
 #' @param concept - the concept representing the latent variable
+#' @param questions - a list of the questions/cols for this concept
 #' @param level - the invariance level ('configural', 'metric', 'scalar', 'strict')
 #'
 #' @return a lavaan object containing the model
-create_lavaan_model <- function(concept, level) {
-  # See comment in first function for definition of concepts
-  model <- paste(sym(concept), '=~', paste(concepts[[concept]], collapse = " + "))
+create_lavaan_model <- function(df, concept, questions, level) {
+  # Format the model for lavaan, which expects LV =~ Q1 + ... + Qn
+  model <- paste(sym(concept), '=~', paste(questions, collapse = " + "))
+  
   if (level == 'configural') {
-    lavObject <- lavaan::cfa(model, data, group = 'group',
-                             std.lv=T, estimator = "DWLS")
+    lavObject <- lavaan::cfa(
+      model,
+      df,
+      group = 'group',
+      std.lv = T,
+      estimator = "DWLS" #this is for likert survey questions, should generalize with param
+    )
   } else {
-    lavObject <- lavaan::cfa(model, data, group = 'group', std.lv=T,
-                             group.equal=equality_constraints[[level]],
-                             estimator = "DWLS")
+    lavObject <- lavaan::cfa(
+      model,
+      df,
+      group = 'group',
+      std.lv=T,
+      group.equal=equality_constraints[[level]],
+      estimator = "DWLS"
+    )
   }
   return(lavObject)
 }
@@ -113,14 +138,17 @@ create_lavaan_model <- function(concept, level) {
 #' Tests measurement invariance of a model on a data frame and group variable
 #' @author Nathan Jordan
 #'
+#' @param df - the dataframe
 #' @param concept - the concept representing the latent variable
+#' @param questions - a list of the questions/cols for this concept
 #' 
 #' @return a list with the following attributes:
 #' model - a lavaan object, or NULL if configural fails
 #' level - the level of invariance established
-invariance_tester <- function(concept) {
+invariance_tester <- function(df, concept, questions) {
+  
   # Run and print configural invariance test
-  configural <- create_lavaan_model(concept, 'configural')
+  configural <- create_lavaan_model(df, concept, questions, 'configural')
   configuralFits <- fitMeasures(configural, fit_indices)
   cat("Configural Invariance Test:\n")
   print(configuralFits)
@@ -132,7 +160,7 @@ invariance_tester <- function(concept) {
   }
   
   # Run and print metric invariance test
-  metric <- create_lavaan_model(concept, 'metric')
+  metric <- create_lavaan_model(df, concept, questions, 'metric')
   metricFits <- fitMeasures(metric, fit_indices)
   cat("\nMetric Invariance Test:\n")
   print(metricFits)
@@ -144,7 +172,7 @@ invariance_tester <- function(concept) {
   }
   
   # Run and print scalar invariance test
-  scalar <- create_lavaan_model(concept, 'scalar')
+  scalar <- create_lavaan_model(df, concept, questions, 'scalar')
   scalarFits <- fitMeasures(scalar, fit_indices)
   cat("\nScalar Invariance Test:\n")
   print(scalarFits)
@@ -162,38 +190,54 @@ invariance_tester <- function(concept) {
 #' visualizing and summarizing the weighted sum score of the variable across the groups
 #' @author Nathan Jordan
 #'
+#' @param df - the dataframe
 #' @param concept - the latent concept to explore
+#' @param questions - a list of the questions/cols for this concept
 #'
 #' @return
-latent_variable_explorer <- function(concept){
-  cfa_object <- invariance_tester(concept)
+latent_variable_explorer <- function(df, concept, questions){
+  # Run invariance tests, return if scalar not established
+  # TODO: create optional param that sets what level we're ok with
+  cfa_object <- invariance_tester(df, concept, questions)
   if (cfa_object$level != 'scalar') {
     return()
   }
-  # Get questions and corresponding factor loadings
-  concept_questions <- concepts[[concept]]
-  num_questions <- length(concept_questions)
+  
+  # Print factor loadings of model
+  # Get corresponding factor loading for each question
+  num_questions <- length(questions)
   factor_loadings <- parameterestimates(cfa_object$model)[['est']][1:num_questions]
   cat("\nFactor loadings:\n")
   cat(factor_loadings)
+  
   # Create a weighted sum score of responses by factor loading
-  sum <- factor_loadings[[1]] * data[[concept_questions[[1]]]]
+  # This is assuming that the factor loading for the first column is a 1.
+  # There are some optional configs in lavaan thatd can change this, so needs
+  # generalizing
+  sum <- factor_loadings[[1]] * df[[questions[[1]]]]
   for (i in 2:num_questions) {
-    sum <- sum + (factor_loadings[[i]] * data[[concept_questions[[i]]]])
+    sum <- sum + (factor_loadings[[i]] * df[[questions[[i]]]])
   }
   weighted_sum <- sum / num_questions
   # Add weighted sum score to copy of the data
-  df <- data
-  df[[sym(concept)]] <- weighted_sum
-  # Print summary statistics
+  df_copy <- df
+  df_copy[[sym(concept)]] <- weighted_sum
+  
+  # Print summary statistics overall
   cat("\n\n\n")
   cat("Summary across all respondents:\n")
-  print(summary(df[[sym(concept)]]))
-  cat("\nPolitical technocrats:\n")
-  print(summary((df |> filter(group == "Political Technocrats"))[[sym(concept)]]))
-  cat("\nOther respondents:\n")
-  print(summary((df |> filter(group == "Rest"))[[sym(concept)]]))
-  df |> select(group, sym(concept)) |> na.omit() |>
+  print(summary(df_copy[[sym(concept)]]))
+  
+  # Print summary statistics for each group
+  num_groups <- levels(df$group)
+  
+  for (i in 1:num_groups) {
+    level <- levels(df$group)[[i]]
+    cat(paste("\n", level, ":\n"))
+    print(summary((df_copy |> filter(group == level))[[sym(concept)]]))
+  }
+
+  df_copy |> select(group, sym(concept)) |> na.omit() |>
     ggplot(aes(eval(sym(concept)), group)) +
     geom_jitter(alpha = 0.15, size = 2.5, color = "#2c7fb8", height=0.1) +
     theme_minimal() + labs(y="", x=sym(concept))
